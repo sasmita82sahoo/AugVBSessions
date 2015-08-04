e853ef9064ca830d4bd4662368daaadfe90d119e
coderchira <ruchira.aggarwal@gmail.com>
2015-08-04 16:46:50 +0530

    CollectionsExercise

---
+++ AugustVBSession/CollectionsExercise.vb
@@ -0,0 +1,9 @@
+﻿Module CollectionsExercise
+    'Create 2 collections
+    '1 collection has 10 names
+    'second collection is empty
+    'code such that : transfer of all the names from one collection to another 
+    'except the one which has your name
+
+
+End Module
